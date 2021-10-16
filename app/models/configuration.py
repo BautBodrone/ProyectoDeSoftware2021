c8dbc4403d0f68ee3e9034e8b97c747b9c1e8814
Alejo Pugliese <alejopugli@gmail.com>
2021-10-16 17:21:31 -0300

    se modifico el atributo cant_filas por rows_per_page

--- app/models/configuration.py
+++ app/models/configuration.py
@@ -14,7 +14,7 @@ class Configuration(db.Model):
 
     __tablename__ = "configurations" 
     id = Column(Integer, primary_key=True)
-    cant_filas = Column(Integer(), default=10)
+    rows_per_page = Column(Integer(), default=10)
     order = Column(ChoiceType(CRITERIOS), default='a..z')
     private_bg_color = Column(String(10), default='#81AE9D')
     private_accent_color = Column(String(10), default='#21A179')
@@ -24,7 +24,7 @@ class Configuration(db.Model):
     public_letters_color = Column(String(10), default='#1E1E24')
     
     def __init__(self, data):
-        self.cant_filas = data["cant_filas"]
+        self.rows_per_page = data["rows_per_page"]
         self.order = data["order"]
         self.private_bg_color = data["private_bg_color"]
         private_accent_color = data["private_accent_color"]
@@ -34,8 +34,8 @@ class Configuration(db.Model):
         public_letters_color = data["public_letters_color"]
     
     def update(self, data):
-        if (data["cant_filas"] != '' and data["cant_filas"] != self.cant_filas):
-            self.cant_filas = data["cant_filas"]
+        if (data["rows_per_page"] != '' and data["rows_per_page"] != self.rows_per_page):
+            self.rows_per_page = data["rows_per_page"]
         if (data["order"] != self.order):
             self.order = data["order"]
         if (data["private_bg_color"] != self.private_bg_color):

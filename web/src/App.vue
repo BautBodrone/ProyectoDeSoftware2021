e852a725ac47e7229b6c0355472f2442eff51b59
BautBodrone <51447970+BautBodrone@users.noreply.github.com>
2021-12-03 16:59:10 -0300

    layout basico

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,114 +1,61 @@
 <template>
-  <img alt="Vue logo" src="@/assets/logo.png" style="height:5rem;float:left;padding-top:30px;margin-left:1%">  
-  <div id="nav">
-    <h1 class=header> Inicio </h1>
-  </div>
-  <div class="navbar" style="background-color:{{ bg_color }};">
-    <router-link to="/">Inicio</router-link>
-    <router-link to="/denuncias">Ver denuncias</router-link>
-    <router-link to="/denunciar">Denunciar</router-link>
-    <router-link to="/zonas-inundables">Zonas inundables</router-link>
-    <router-link to="/puntos-de-encuentro">Puntos de encuentros</router-link> 
-    <router-link to="/recorridos">Recorridos</router-link>
-  </div>
+  
+  
+  <header class="top-bar spread">
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;" >
+      <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <img alt="Vue logo" src="@/assets/logo.png" width="100" height="100"> 
+          <ul class="navbar-nav my-2 my-lg-0 me-sm-0 my-sm-0">
+            <li class="nav-item">
+              <router-link class="nav-link active" to="/" aria-current="page">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link active" to="/denunciar" aria-current="page">Denuncias</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link active" to="/zonas-inundables" aria-current="page">Zonas Inundables</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link active" to="/puntos-de-encuentro" aria-current="page">Puntos</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link active" to="/recorridos" aria-current="page">Recorrido</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
   <router-view/>
 </template>
 
-<script>
-export default {
-  name: 'App',
-  data() {
-    return {
-      pagination: 'Integer',
-      bg_color: 'String',
-      accent_color: 'String',
-      color: 'String'  
-    };
-  },
-  created() {
-    fetch(process.env.VUE_APP_ROOT_API+'/configuration-publica/').then((response) => {
-        return response.json();
-    }).then((json) => {
-        this.pagination = json.rows_per_page;
-        this.order = json.order;
-        this.bg_color = json.public_bg_color;
-        this.accent_color = json.public_accent_color;
-        this.color = json.public_letters_color;
-    }).catch((e) => {
-        console.log(e)
-    })
-  }
-};
-</script>
-<style>
-.navbar a {
-  margin-bottom:1rem;
-  text-decoration: none;
-  font-size: 1em;
-  display: block;
-  width: 100%;
-  height: 3.5em;
-}
-.navbar {
-  font-family: 'Montserrat', sans-serif;
-  float: left;
-  width: 15%;
-  padding-left: 1%;
-  padding-top: 1%;
-  padding-right: 1%;
-  margin-bottom: 1em;
-  margin-top: 1em;
-  text-align: left;
-  transition: 0.5s;
-  overflow-x: hidden;
-  white-space: nowrap;
-}
-
-.navbar a.router-link-exact-active {
-  background-color: #428db9;
-}
-
-h3 {
-  margin: 40px 0 0;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-
-.header {
-  text-align: center;
-  font-size: 30px;
-}
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+// <script>
+// export default {
+//   name: 'App',
+//   data() {
+//     return {
+//       pagination: 'Integer',
+//       bg_color: 'String',
+//       accent_color: 'String',
+//       color: 'String'  
+//     };
+//   },
+//   created() {
+//     fetch(process.env.VUE_APP_ROOT_API+'/configuration-publica/').then((response) => {
+//         return response.json();
+//     }).then((json) => {
+//         this.pagination = json.rows_per_page;
+//         this.order = json.order;
+//         this.bg_color = json.public_bg_color;
+//         this.accent_color = json.public_accent_color;
+//         this.color = json.public_letters_color;
+//     }).catch((e) => {
+//         console.log(e)
+//     })
+//   }
+// };
+// </script>
--- web/src/main.js
+++ web/src/main.js
@@ -1,5 +1,19 @@
-import { createApp } from 'vue'
+
+
+//createApp(App).use(router).mount('#app')
+
+import {createApp} from 'vue'
+import BootstrapVue3 from 'bootstrap-vue-3'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+// Optional, since every component import their Bootstrap funcionality
+// the following line is not necessary
+// import 'bootstrap'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
+const app = createApp(App).use(router).mount('#app')
+app.use(BootstrapVue3)
+app.mount('#app')
